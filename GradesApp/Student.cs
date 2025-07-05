8f62330e9e7ba11e56f8de629196bc9836a838ac
Thai Vo <tahrbo@gmail.com>
2025-07-04 20:00:51 -0500

    Start

---
+++ GradesApp/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradesApp
+{
+    public class Student
+    {
+        string Name;
+        int ID;
+        List<double> Grades = new List<double>();
+    }
+}
