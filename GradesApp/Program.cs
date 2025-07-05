aac2840912f809418931ab6586b75b4b9111a2b4
Thai Vo <tahrbo@gmail.com>
2025-07-04 20:36:22 -0500

    Working on list

--- GradesApp/Program.cs
+++ GradesApp/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+
+namespace GradesApp;
+{    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        List<Student> students = new List<Student>();
+
+        var s1 = new Student("J Lennon", 1);
+        var s2 = new Student("P McCartney", 2);
+        var s3 = new Student("G Harrison", 3);
+        var s4 = new Student("R Starr", 4);
+
+
+        }
+
+    }
+}
+
+
+
+
+
