cd75356c13e95c9023d8c4dce381d57a9085d21a
Thai Vo <tahrbo@gmail.com>
2025-07-05 00:26:02 -0500

    Ready

---
+++ GradesApp/Course.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradesApp
+{
+    public class Course
+    {
+        public string CourseName;
+        public string CourseCode;
+
+        public List<Student> EnrolledStudents = new List<Student>();
+
+        public Course(string courseName, string courseCode)
+        {
+            CourseName = courseName;
+            CourseCode = courseCode;
+
+            EnrolledStudents = new List<Student>();
+        }
+
+        public void EnrollStudents(Student student) 
+        {
+            if (!EnrolledStudents.Contains(student))
+                {
+                EnrolledStudents.Add(student);                
+                }
+        }
+
+    }
+}
